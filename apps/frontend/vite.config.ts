58fb7a8a607740c22183148f2a52be57d2c78b06
JackatDJL <71508487+JackatDJL@users.noreply.github.com>
2026-02-11 22:31:38 +0100

    123
    Signed-off-by: JackatDJL <71508487+JackatDJL@users.noreply.github.com>

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,16 +3,10 @@ import { defineConfig } from 'vite'
 import { nitro } from 'nitro/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 
 export default defineConfig({
   resolve: {
     alias: {
-      'tsr:routes-manifest': path.join(
-        path.dirname(fileURLToPath(import.meta.url)),
-        'node_modules/@tanstack/react-start-router-manifest/dist/esm/index.js'
-      ),
       'node:async_hooks': 'async_hooks',
       'node:stream': 'stream',
       'node:stream/web': 'stream/web'
